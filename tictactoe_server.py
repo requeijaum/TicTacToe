e3ab384bee1a1665b7066adbf9008cc50d3b2c5e
Rafael Felix Silva Requião <rafaelfrequiao@gmail.com>
2019-03-28 19:47:23 -0300

    Commit do servidor no iHack - preciso fazer tweaks de algumas bobagens - temporização.

--- tictactoe_server.py
+++ tictactoe_server.py
@@ -24,6 +24,8 @@ game_condition = False
 msg = "Testando 1234"           # enviar ao cliente - mensagem inicial não-nula
 recv = ""                       # recebido do cliente
 
+askPlayer2 = True
+
 thread_list = {
     "socketCriado"              : None,
     "buscadorClientes"          : None,
@@ -36,7 +38,7 @@ def createServerSocket(s):
     global game_condition
 
     # s = socket.socket()       # Create a socket object
-    host = '127.0.0.1'          # socket.gethostname() # Get local machine name
+    host = 'ihack-de-rafael.local'          # socket.gethostname() # Get local machine name
     port = 50790                # Reserve a port for your service.
 
     print('Server started!')
@@ -96,17 +98,28 @@ def novoCliente():
     global c
     global addr
 
+    #first setup
+    msg = "Olá!"
+
     while True:
-        time.sleep(1)   # tentar encher buffer?
-        data_received = c.recv(1024)
-        recv = data_received.decode()
-        #if len(recv) > 0 :
-        #    print(addr, ' >> ', recv)
-        print(addr, ' >> ', recv)
+        time.sleep(0.1)   # tentar encher buffer? serve pra escrever \n corretamente - no cliente5
+        try:
+            data_received = c.recv(1024)
+            pass
+
+        except:
+            print("Ocorreu um erro ao processar o comando recebido!")
+            exit
+
+        if len(data_received.decode()) > 0:
+            recv = data_received.decode()
+            if recv != "invalido":
+                print(addr, ' >> ', recv)
+        
 
         #debug
         #enviarCliente()
-        imprimir("memes")
+        #imprimir("")
 
         #if not data_received: break        
         #do some checks and if msg == someWeirdSignal: break:
@@ -128,11 +141,32 @@ def enviarCliente() :
     global msg         # usar argumento
     global c
     
-    c.send(msg.encode())
-    
-    #print(msg)
-    #exit  #forçar saída
+    try:
+        time.sleep(0.5) # na verdade a magica da impressao e envio de dados fica aqui... ajustar pra esse valor sempre funciona
+        #print(str(type(c)))
+        
+        if type(c) != type(None) :
+            c.send(msg.encode())
+        
+        pass
 
+    except:
+        print("\nDEBUG: Algum erro ocorreu ao processar o envio...\n")
+        pass
+
+    '''
+    print(getattr(s, "getpeername"))
+
+    if not type(c) == "NoneType" : 
+        if getattr(s, "getpeername") :
+            c.send(msg.encode())
+        else:
+            print("Não foi possível enviar: " + msg)
+    else :
+        print("\n>> Calma! O socket ainda não foi instanciado!\n")
+        #print(msg)
+        #exit  #forçar saída
+    '''
 
 def imprimir(texto):
     global msg
@@ -166,7 +200,7 @@ def procurarClientes():
 
     while game_condition == False:
         print("\n Não encontramos clientes disponiveis...")
-        time.sleep(5)
+        time.sleep(3)
 
     # \/\/\/\/ comentei isso aqui tudo pra poder debuggar o socket... \/\/\/\/
 
@@ -184,11 +218,11 @@ def full():
 
 
 def board_display(score):
-    imprimir(' '+score[6]+ ' | '+ score[7]+ ' | '+ score[8])
-    imprimir('---+---+---')
-    imprimir(' '+score[3]+ ' | '+ score[4]+ ' | '+ score[5])
-    imprimir('---+---+---')
-    imprimir(' '+score[0]+ ' | '+ score[1]+ ' | '+ score[2])
+    imprimir(' '+score[6]+ ' | '+ score[7]+ ' | '+ score[8] )#+ "\n")
+    imprimir('---+---+---'  )#+ "\n")   
+    imprimir(' '+score[3]+ ' | '+ score[4]+ ' | '+ score[5] )#+ "\n")
+    imprimir('---+---+---' )#+ "\n")
+    imprimir(' '+score[0]+ ' | '+ score[1]+ ' | '+ score[2] )#+ "\n")
 
 
 def check(char, posi1, posi2, posi3):
@@ -216,19 +250,29 @@ def checkall(char):
     return False
 
 
+def comandoSistema(cmd):
+    global msg
+    msg = "/cmd " + cmd
+    enviarCliente()
+    os.system(cmd)
+
 def Main():
 
     global thread_list
     global game_condition
     global score
+    global msg
+    global recv
+    global askPlayer2
 
+    comandoSistema("clear")
     # imprimir('Tic Tac Toe\n'.center(70))
     banner = r'''
-                _____  _  ____     _____  ____  ____     _____  ____  _____
+                 _____  _  ____     _____  ____  ____     _____  ____  _____
                 /__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/
-                / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \
-                | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_
-                \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\ '''
+                  / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \
+                  | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_
+                  \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\ '''
 
     imprimir(banner)
     imprimir('\nThe board is numbered in the form of the numpad keyboard as printed below.\n')
@@ -247,12 +291,12 @@ def Main():
 
     while game_condition :
 
-        imprimir('\nChoose your cells accordingly.The game will begin in 10 seconds.')
+        imprimir('\nChoose your cells accordingly.\nThe game will begin in 10 seconds.')
         time.sleep(10)
 
         score = [' ']*9
         while True:
-            os.system('clear')
+            comandoSistema('clear')
             board_display(score)
             if full():                
                 break
@@ -261,17 +305,20 @@ def Main():
                 break
             while True:
                 try:
+                    msg = "Waiting Player X"
+                    enviarCliente()
                     player1 = int(input('\nPlayer X, choose your position: '))
                     if score[player1-1] != 'X' and score[player1-1] != 'O':
                         score[player1-1] = 'X'
                         break
                     else:
                         imprimir('\nThis position is already taken, please try again.')
-                        continue
+                        #continue
+                        break
                 except:
                     imprimir('\nKindly enter a valid value')
                     continue
-            os.system('clear')
+            comandoSistema('clear')
             board_display(score)
             if checkall('X'):
                 imprimir('\nPlayer X is the winner')
@@ -279,19 +326,47 @@ def Main():
                 break
             if full():
                 break
-            while True:
+            #special stuff
+            print("\n\n > Waiting player O")    
+            
+            while askPlayer2:
                 try:
-                    player2 = int(input('\nPlayer O, choose your position: '))
-                    if score[player2-1] != 'X' and score[player2-1] != 'O':
-                        score[player2-1] = 'O'
+                    while len(recv) < 1 : #or recv == "invalido" :   # a mágica está aqui e em recv = "invalido"
+                        #print("\n\n > Waiting player O")
+                        time.sleep(3)
+                        #time.sleep()
+                        #continue
+
+                    #player2 = int(input('\nPlayer O, choose your position: '))
+                    msg = "\nPlayer O, choose your position: "
+                    enviarCliente()
+                    time.sleep(5)
+                    #print("DEBUG: recv = " + recv)
+                    #if len(recv) > 0 and int(recv) in [0,1,2,3,4,5,6,7,8,9] :
+                    if len(recv) == 1:
+                        player2 = int(recv)
+
+
+
+                        if score[player2-1] != 'X' and score[player2-1] != 'O':
+                            score[player2-1] = 'O'
+                            break
+                        else:
+                            if len(recv) == 0 or len(recv) == 1 :
+                                imprimir('\nThis position ' + str(recv) + ' is already taken, please try again.')
+                                #time.sleep(5)
+                            continue
+
                         break
-                    else:
-                        imprimir('\nThis position is already taken, please try again.')
-                        continue
+                    #recv = "100"    # setar pra um valor fora do escopo [0-9]... tem que ser um número. Nunca pode ser uma string vazia!
+                    #break
+                    pass  # talvez isso quebre tudo!
                 except:
-                    imprimir('\nKindly enter a valid value')
+                    #if len(recv) == 0 : 
+                    imprimir('\nKindly enter a valid value - not ' + str(recv))
+                    #time.sleep(5)
                     continue
-            os.system('clear')
+            comandoSistema('clear')
             board_display(score)
             if checkall('O'):
                 imprimir('\nPlayer O is the winner')
@@ -303,6 +378,8 @@ def Main():
         # forçar fechar socket
         if thread_list["novoCliente"].is_alive() :
             s.close()
+        msg = "O servidor está decidindo se ele quer jogar de novo..."
+        enviarCliente()
         again = input('\nDo you want to play again? Enter Yes or No: ')
         if again.lower() == 'yes':
             continue
@@ -315,6 +392,8 @@ def Main():
             time.sleep(1)
             imprimir('           1..')
             time.sleep(1)
+            msg = "/cmd quit"
+            enviarCliente()
             break
         else:
             imprimir('\nEnter a correct choice')
